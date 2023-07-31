e34a915ab9aecca61e986ab624dfcd20d2e493a7
Matthew <jiaomatthew222@gmail.com>
2023-07-31 16:49:00 -0400

    update

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,9 +1,18 @@
 export const areNumbersEqual = (a: number, b: number) => {
   return a === b;
-}
+};
 
 export const getSum = (numbers: number[]): number => {
   return numbers.reduce((acc, num) => {
     return acc + num;
-  }, 0)
-}
+  }, 0);
+};
+
+export const getSum2 = (numbers: number[]): number => {
+  return (
+    1 +
+    numbers.reduce((acc, num) => {
+      return acc + num;
+    }, 0)
+  );
+};
