61caf316c95e5742e42be51a742a27928b360587
Matthew <jiaomatthew222@gmail.com>
2023-08-14 13:41:48 -0400

    update

--- scripts/build-if-package-version-different.sh
+++ scripts/build-if-package-version-different.sh
@@ -5,4 +5,9 @@ local_package_version=$(jq -r '.version' package.json);
 
 latest_version=$(npm view --silent ${package_name} version)
 
+echo "here"
+echo $package_name
+echo $local_package_version
+echo $latest_version
+
 if [ "$local_package_version" != "$latest_version" ]; then yarn lint && yarn build; else :; fi
